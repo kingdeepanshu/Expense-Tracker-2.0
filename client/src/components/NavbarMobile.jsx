b4cf6f6e6a7ae0da677b1d3d4196cce4c7ead6f3
kingdeepanshu <king.deepanshu24@gmail.com>
2024-06-30 17:38:06 +0530

    header

--- client/src/components/NavbarMobile.jsx
+++ client/src/components/NavbarMobile.jsx
@@ -7,10 +7,10 @@ function NavbarMobile() {
       <h1 className='text-3xl font-bold'>Expenses</h1>
 
       <menu className="w-full flex justify-between text-indigo-600">
-        <h1 className="text-lg my-3 text-left hover:font-bold bg-white px-2 rounded-lg"><Link to="/">DashBoard</Link></h1>
-        <h1 className="text-lg my-3 text-left hover:font-bold bg-white px-2 rounded-lg"><Link to="/expenses">Expenses</Link></h1>
-        <h1 className="text-lg my-3 text-left hover:font-bold bg-white px-2 rounded-lg"><Link to="/summary">Summary</Link></h1>
-        <h1 className="text-lg my-3 text-left hover:font-bold bg-white px-2 rounded-lg"><Link to="/activities">Activities</Link></h1>
+        <h1 className="text-lg my-3 text-left hover:font-bold bg-white px-2 rounded-sm"><Link to="/">DashBoard</Link></h1>
+        <h1 className="text-lg my-3 text-left hover:font-bold bg-white px-2 rounded-sm"><Link to="/expenses">Expenses</Link></h1>
+        <h1 className="text-lg my-3 text-left hover:font-bold bg-white px-2 rounded-sm"><Link to="/summary">Summary</Link></h1>
+        <h1 className="text-lg my-3 text-left hover:font-bold bg-white px-2 rounded-sm"><Link to="/activities">Activities</Link></h1>
       </menu>
     </div>
   )
